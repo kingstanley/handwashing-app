270224fff4d73fb0d25ed33b5b1909b5fb35c01c
kingstanley <32240704+kingstanley@users.noreply.github.com>
2020-07-15 00:06:53 +0100

    got rollup config working

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
-	main {
+	/* main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -20,11 +20,11 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
+	} */
 
-	@media (min-width: 640px) {
+	/* @media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
-	}
+	} */
 </style>
