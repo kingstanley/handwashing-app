ee572c713eca9be0efc6e659cf95106184b27668
kingstanley <32240704+kingstanley@users.noreply.github.com>
2020-07-23 11:22:48 +0100

    2-HTML

---
+++ src/Timer.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Progressbar from './Progressbar.svelte'
+</script>
+<style>
+    h2{margin: 0;}
+    .start{
+       background-color: rgb(154, 73,73);
+       width: 100%;
+       margin: 10px 0;
+    /* color: #fff;
+    height: 20px; */
+    }
+</style>
+<div bp="grid">
+ <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: </h2>
+</div>
+<Progressbar />
+
+<button class="start">Start</button>
