8d3269a93d96cfe7aec46b5677c4ca65f8bb86ab
kingstanley <32240704+kingstanley@users.noreply.github.com>
2020-07-23 21:26:25 +0100

    Commiting to html branch

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script>
-import Timer from './Timer.svelte';
-import HandTool from './HowTo.svelte';
-	export let name;
+  import Timer from './Timer.svelte'
+  import HandTool from './HowTo.svelte'
+  export let name
 </script>
 
-<main>
-	<h1>Handwashing App!</h1>
-	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
-	<Timer />
-	<HandTool />
-</main>
-
 <style>
-	h1,h3{text-align: center;}
-</style> 
+  h1,
+  h3 {
+    text-align: center;
+  }
+</style>
+
+<main>
+  <h1>Handwashing App!</h1>
+  <!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+  <Timer />
+  <HandTool />
+</main>
--- src/Progressbar.svelte
+++ src/Progressbar.svelte
@@ -1,16 +1,16 @@
 <style>
-    .progress-container{
-        margin: 20px 0;
-        border: solid rgb(194, 194, 194) 1px;
-    }
-    .progress-bar{
-        height: 30px;
-background-color: rgb(77, 199, 55);
-    }
+  .progress-container {
+    margin: 20px 0;
+    border: solid rgb(194, 194, 194) 1px;
+  }
+  .progress-bar {
+    height: 30px;
+    background-color: rgb(77, 199, 55);
+  }
 </style>
+
 <div bp="offset-5@md 4@md 12@sm">
-<div class="progress-container">
-<div class="progress-bar" style="width: 20%">
-</div>
-</div>
+  <div class="progress-container">
+    <div class="progress-bar" style="width: 20%" />
+  </div>
 </div>
